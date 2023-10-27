5e1ab1e0c11ff5433d9eac8fd88a844fd3a8ec68
mignonnecilliers <mignonne@softwareworx.co.za>
2023-10-27 15:57:15 +0200

    #01 created sentence component

--- sentence-builder/src/app/app.module.ts
+++ sentence-builder/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SentencesComponent } from './components/sentences/sentences.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SentencesComponent
   ],
   imports: [
     BrowserModule,

b4d756cdc5a706ca9048d84a7397cbab34828d3f
mignonnecilliers <mignonne@softwareworx.co.za>
2023-10-27 19:00:39 +0200

    #01 created getwords api call

--- sentence-builder/src/app/services/sentences.service.ts
+++ sentence-builder/src/app/services/sentences.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { environment } from './../../environments/environment'
+import { catchError, map } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SentencesService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  // Method to get all previously submitted sentences from the backend
+  getWordTypes() {
+    const url = `${environment.apiUrl}/wordTypes`; // Replace with your API endpoint
+    return this.httpClient.get<string[]>(url);
+  }
 }
